b4d8437412e775946af58a052b370da01bcadd1d
ilayda <ilaydaozken@gmail.com>
2023-08-06 19:30:36 +0300

    .

--- CasgemRapidApi/Controllers/BookingController.cs
+++ CasgemRapidApi/Controllers/BookingController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CasgemRapidApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CasgemRapidApi.Controllers
 {
     public class BookingController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index(string adult="1", string checkinDate="2023-9-7", string cityID= "-553173", string checkoutDate="2023-09-28", string roomNumber="1", string child="1")
         {
-            return View();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v2/hotels/search?order_by=popularity&adults_number={adult}&checkin_date={checkinDate}&filter_by_currency=USD&dest_id={cityID}&locale=en-gb&checkout_date={checkoutDate}&units=metric&room_number={roomNumber}&dest_type=city&include_adjacency=true&children_number={child}&page_number=0&children_ages=5%2C0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "f2aca2c6b5msh6e1ed4b2f16a931p170f45jsndf22767a5549" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<HotelListModel>(body);
+                return View(value.results.ToList());
+            }
         }
     }
 }
---
+++ CasgemRapidApi/Models/HotelListModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CasgemRapidApi.Models
+{
+    public class HotelListModel
+    {
+        public Result[] results { get; set; }
+    
+        public class Result
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public float reviewScore { get; set; }
+        }
+    }
+}
